aa2aae1ac114d2c979315e8ffb9d4480003b003a
Mohammed Abdelmegeed <m.maguid9@hotmail.com>
2023-04-30 21:59:52 +0300

    Updated car orientations

--- client.py
+++ client.py
@@ -51,11 +51,9 @@ motorcycle_loc.center = random.randint(width/2 - road_width/2 + roadmark_width*2
 ferrari = pygame.image.load("./Car images/Ferrari img.png")
 ferrari_loc = ferrari.get_rect()
 ferrari_loc.center = random.randint(width/2 - road_width/2 + roadmark_width*2 -6,width/2 + road_width/2 - roadmark_width*2 -25) , 0
-# cars = [motorcycle,ferrari]
-# car2 = cars[random.randint(0,1)]
 
 
-def main():
+def main(playerName):
     pygame.init()
     run = True
     lane_marker_move_y = 0
@@ -86,10 +84,99 @@ def main():
         if lane_marker_move_y >= marker_height * 2:
             lane_marker_move_y = 0
         label_font = pygame.font.Font(None, 24)
-        label_surface = label_font.render('7ambozoo', True, (255,255,255))
+        label_surface = label_font.render(playerName, True, (255,255,255))
         label_rect = label_surface.get_rect()
-        label_rect.topleft = (p.x-18, p.y+90)
+        label_rect.center = (p.x+15, p.y+100)
         redrawWindow(win, p, p2,lane_marker_move_y,motorcycle,motorcycle_loc,ferrari,ferrari_loc,label_rect,label_surface)
-        
+ 
+   
+input_text = ''
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Input Field')
+background_image = pygame.image.load("3555572.jpg").convert()
 
-main()
+
+def homeScreen():
+    pygame.init()
+    # define colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
+    # set up the font
+    font = pygame.font.Font(None, 32)
+
+    # set up the input field
+    input_box = pygame.Rect(width/2 -50, height/2 +50, 200, 45)
+    global input_text
+
+    # set up the submit button
+    submit_button = pygame.Rect(input_box.x + 10, input_box.y +70, 100, 50)
+
+    # set up the label for the input field
+    label_font = pygame.font.Font(None, 24)
+    label_surface = label_font.render('Player Name', True, WHITE)
+    label_rect = label_surface.get_rect()
+    label_rect.topleft = (input_box.x - 110, input_box.y + 15)
+
+    # set up the label for the submit button
+    submit_font = pygame.font.Font(None, 24)
+    submit_surface = submit_font.render('Submit', True, WHITE,(8, 92, 209))
+    submit_rect = submit_surface.get_rect()
+    submit_rect.topleft = (submit_button.x +15, submit_button.y+15)        
+    home_running = True
+    while home_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                home_running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    # save the text when user hits "return"
+                    input_text = input_text.strip() # remove whitespace from input_text
+                    print(input_text)
+                    # input_text = ''
+                    home_running = False
+                    main(input_text)
+                    break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if submit_button.collidepoint(event.pos):
+                    # save the text when user clicks submit button
+                    input_text = input_text.strip() # remove whitespace from input_text
+                    print(input_text)
+                    # input_text = ''
+                    home_running = False
+                    main(input_text)
+                    break
+            # update the input_text variable
+            if event.type == pygame.KEYDOWN:
+                if event.unicode.isalnum():
+                    input_text += event.unicode
+                elif event.key == pygame.K_BACKSPACE:
+                    input_text = input_text[:-1]
+                    
+        #Background
+        screen.blit(background_image, (0, 0))
+
+        # draw the input field, label, and submit button
+        pygame.draw.rect(screen, WHITE, input_box)
+        screen.blit(label_surface, label_rect)
+        pygame.draw.rect(screen, (8, 92, 209), submit_button)
+        screen.blit(submit_surface, submit_rect)
+
+        # render the text
+        input_surface = font.render(input_text, True, BLACK)
+        screen.blit(input_surface, (input_box.x + 5, input_box.y + 5))
+        
+        #display title
+        title_font = pygame.font.Font(None, 64)
+        title_surface = title_font.render('PyDrift', True, BLACK)
+        title_rect = title_surface.get_rect()
+        title_rect.midtop = (width / 2, 50)
+        screen.blit(title_surface, title_rect)
+
+        # update the display
+        pygame.display.flip()
+    
+
+
+homeScreen()
+# main(input_text)
